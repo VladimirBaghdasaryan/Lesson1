07075b897d7e5a7281a93221d188186558fd5951
Vlo Baxdasaryan <vlovlo1994@mail.ru>
2023-06-02 19:18:49 +0400

    Add project files.

---
+++ Lesson1/Program.cs
@@ -0,0 +1,122 @@
+﻿namespace Lesson1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //byte b = 120;
+            //byte bb = 98;
+            //Console.WriteLine(bb);
+            //Console.WriteLine(b+bb);
+
+            //sbyte s = 125;
+            //sbyte ss = 110;
+            //sbyte sss = (sbyte)(s - ss);
+            //Console.WriteLine(sss);
+
+            //short s1 = 12000;
+            //short s2 = 2;
+            //short s3 = (short)(s1 * s2);
+            //Console.WriteLine(s3);
+            //Console.WriteLine(s1 / s2);
+
+            //int i = 15890;
+            //int ii = 12234343;
+            //int iii = i + ii;
+            //Console.WriteLine(iii);
+
+            //long l = 1312312312432000000;
+            //long ll= 3231231241341;
+            //Console.WriteLine(l-ll);
+
+            //float f = 5.454353333333f;
+            //float ff = 4.54654654754f;
+            //float fff = f * ff;
+            //Console.WriteLine(fff);
+
+            //double d = 23.45465466456;
+            //double dd = 43.57568568;
+            //Console.WriteLine( d/dd);
+
+            //decimal d1 = 342.4324232432m;
+            //decimal d2= 432.432432432432423m;
+            //decimal d3 = d2 - d1;
+            //Console.WriteLine(d3);
+
+            //char ch = 'A';
+            //char chh = 'B';
+            //Console.WriteLine(ch+chh);
+
+            //bool b1 = true;
+            //bool b2 = false;
+            //bool b3 = (b1+b2);
+
+
+            //string name = "Vladimir ";
+            //string surname = "Baghdasaryan";
+            //Console.WriteLine(name+surname);
+
+
+            //Console.WriteLine("Qani tarekan eq?");
+            //int age = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Duq cnvel eq "+(2023-age)+" tvakanin");
+
+
+            //Console.Write("Mutqagreq arajin tivy: ");
+            //int num1=Convert.ToInt32 (Console.ReadLine());
+            //Console.Write("Mutqagreq erkrordy: ");
+            //int num2 = Convert.ToInt32 (Console.ReadLine());
+            //int sum = num1 + num2;
+            //Console.WriteLine("Sum = "+sum);
+
+            //Console.Write("Mutqagreq tiv: ");
+            //int tiv = Convert.ToInt32(Console.ReadLine());
+            //if (tiv > 100)
+            //{
+            //    Console.WriteLine("Mutqagrq 100-ic poqr tiv");
+            //}
+            //else
+            //{
+            //    Console.WriteLine(tiv);
+            //}
+
+            //int numm = Convert.ToInt32(Console.ReadLine());
+            //if (numm > 20 && numm < 200)
+            //{
+            //    Console.WriteLine(true);
+            //    if (numm % 2 == 0)
+            //    {
+            //        Console.WriteLine(numm + " % 2 = 0");
+
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine(numm + " % 2 != 0");
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine(false);
+            //}
+
+            string name = Console.ReadLine();
+            if (name == "Ashot" || name == "Hasmik")
+            {
+                Console.WriteLine(true);
+            }
+            else
+            {
+                Console.WriteLine(false);
+            }
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
